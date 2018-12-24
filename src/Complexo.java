7a6de7ee013a1387c1404c7955e57b3a3ec58493
joao <joao182cx@gmail.com>
2018-12-24 21:40:04 -0200

    Criação dos construtores e dos métodos para a classe Complexo.

--- src/Complexo.java
+++ src/Complexo.java
@@ -1,4 +1,38 @@
 
 public class Complexo {
+	
+	private double parteReal;
+	private double parteImaginaria;
+	private final static String i = "i";
+	
+	public Complexo() {
+		this(1, 1);
+	}
+	
+	public Complexo(double parteReal, double parteImaginaria) {
+		super();
+		this.parteReal = parteReal;
+		this.parteImaginaria = parteImaginaria;
+	}
+
+	public double getParteReal() {
+		return parteReal;
+	}
+
+	public void setParteReal(double parteReal) {
+		this.parteReal = parteReal;
+	}
+
+	public double getParteImaginaria() {
+		return parteImaginaria;
+	}
+
+	public void setParteImaginaria(double parteImaginaria) {
+		this.parteImaginaria = parteImaginaria;
+	}
+
+	public String getI() {
+		return i;
+	}
 
 }
